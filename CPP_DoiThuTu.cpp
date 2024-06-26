760793dbc81293ba3c32c7ab23a915433e113dcc
SPC <SPC@20-LAB402>
2024-06-26 20:27:00 +0700

    Add project files.

---
+++ CPP_DoiThuTu.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int so[5];
+	for (int i = 0; i< 5 ; i++)
+	{
+		cout <<"nhap so thu "<< i + 1 << ":";
+		cin >> so[i];
+	}
+	
+	return 0;
+}
---
+++ enc_temp_folder/cf7ff09b5f105ebe7c822ade631d1b81/CPP_DoiThuTu.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int so[5];
+	for (int i = 1; i<5 ; i++)
+	{
+		cout <<"nhap so thu 1 "<< i +1<< ":";
+		cin >> so[i];
+	}
+	
+	return 0;
+}
