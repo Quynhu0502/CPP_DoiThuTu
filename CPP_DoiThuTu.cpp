a5c6ed9bbe6385a785006096428ec6f4276cb26d
Quynhu0502 <thaiquynhnhu.210502@GMAIL.COM>
2024-07-01 19:09:14 +0700

    Update CPP_DoiThuTu.cpp
    sua bai

--- CPP_DoiThuTu.cpp
+++ CPP_DoiThuTu.cpp
@@ -1,14 +1,45 @@
 
 #include <iostream>
 using namespace std;
+
+void SWnum(int& a, int& b)
+{
+	int c = a;
+	a = b;
+	b = c;
+}
+
 int main()
 {
-	int so[5];
+	
+	int so [5];
 	for (int i = 0; i< 5 ; i++)
 	{
-		cout <<"nhap so thu "<< i + 1 << ":";
+		cout <<"nhap so thu "<< i << ":";
 		cin >> so[i];
 	}
-	
+	for (int x : so)
+		cout << x << " ";
+	int vitriMax = 0;
+	int soMax = 0;
+	for (int  i = 0; i < 5 ; i++)
+	{
+		if (soMax < so[i])
+		{
+			soMax = so[i];
+			vitriMax = i;
+		}
+		
+		
+	}
+	SWnum(so[0], so[vitriMax]);
+	{
+		cout << "\nSaukhiWap \n";
+ 
+	}
+	for (int i : so)
+	{
+		cout << i << " ";
+	}
 	return 0;
 }
